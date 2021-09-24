168bb0431f5011c2c91e18180bcd5f5be45794d7
MrMrRPG <nng5998@gmail.com>
2021-09-23 22:33:24 -0700

    It's working? Idk anymore

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key
 export const API_KEY =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
--- static/js/logic.js
+++ static/js/logic.js
@@ -4,13 +4,34 @@ import { API_KEY } from "./config.js";
 // console.log to confirm the key has been imported
 // console.log(API_KEY);
 
+let url = API_KEY;
+
 // GET request on API
-d3.json(API_KEY).then(function (data) {
+d3.json(url).then(function (data) {
   createFeatures(data.features);
   //   console.log(API_KEY);
 });
 
 function createFeatures(earthquakeData) {
+  function magnitudeColor(depth) {
+    if (depth <= 10) return "#0CE49F";
+    else if (depth <= 30) return "#0EE40C";
+    else if (depth <= 50) return "#D5E40C";
+    else if (depth <= 70) return "#E4760C";
+    else if (depth <= 90) return "#E40C0C";
+  }
+
+  function style(data) {
+    return {
+      opacity: 0.5,
+      fillOpacity: 0.5,
+      weight: 3.5,
+      fillColor: magnitudeColor(data.geometry.coordinates[2]),
+      color: "white",
+      radius: data.properties.mag * 9,
+    };
+  }
+
   function onEachFeature(feature, layer) {
     layer.bindPopup(
       `<h3>${feature.properties.place}</h3><hr><p>${new Date(
@@ -20,7 +41,11 @@ function createFeatures(earthquakeData) {
   }
 
   var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlong) {
+      return L.circleMarker(latlong);
+    },
     onEachFeature: onEachFeature,
+    style: style,
   });
 
   createMap(earthquakes);
@@ -41,30 +66,42 @@ function createMap(earthquakes) {
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
   });
 
-  // Create a baseMaps object.
   var baseMaps = {
     "Street Map": street,
     "Topographic Map": topo,
   };
 
-  // Create an overlay object to hold our overlay.
   var overlayMaps = {
     Earthquakes: earthquakes,
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
   var myMap = L.map("map", {
     center: [37.09, -95.71],
-    zoom: 1,
+    zoom: 5,
     layers: [street, earthquakes],
   });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
   L.control
     .layers(baseMaps, overlayMaps, {
       collapsed: false,
     })
     .addTo(myMap);
 }
+
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function (map) {
+  let div = L.DomUtil.create("div", "info legend"),
+    depth = [10, 30, 50, 70, 90];
+  colors = ["#0CE49F", "#0EE40C", "#D5E40C", "#E4760C", "#E40C0C"];
+
+  for (var i = 0; i < depth.length; i++) {
+    div.innerHTML +=
+      "<i style= 'background: " +
+      colors[i] +
+      "'></i> " +
+      depth[i] +
+      (depth[i + 1] ? "&ndash;" + depth[i + 1] + "<br>" : "+");
+  }
+  return div;
+};
+// Adding legend to the map
+legend.addTo(myMap);
